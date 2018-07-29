4c61c13747d0e567d9b2c00b528a87ba4abc64a6
Shama <shama.hegde@gmail.com>
2018-07-29 15:03:36 +0530

    with modal

---
+++ js/carousel.js
@@ -0,0 +1,14 @@
+
+    window.onload=function(){
+      
+      $(".single-item").slick({
+        dots: true,
+        infinite: true,
+      slidesToShow: 6,
+      slidesToScroll:4,
+      cssEase: "ease",
+      autoplay: true,
+      autoplaySpeed: 3000,
+      });
+        
+          }
